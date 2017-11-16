0ce299b1226ca8964c92694a944e9905fb3d9843
Ameet Vadhia <ameet01@gmail.com>
2017-11-15 17:56:55 -0800

    finish phase 2

--- Pokedex/frontend/pokedex.jsx
+++ Pokedex/frontend/pokedex.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {receiveAllPokemon, requestAllPokemon} from './actions/pokemon_actions';
+import {fetchAllPokemon} from './util/api_util';
+import configureStore from './store/store';
+import {selectAllPokemon} from './reducers/selectors';
 
 document.addEventListener('DOMContentLoaded', () => {
+  window.receiveAllPokemon = receiveAllPokemon;
+  window.fetchAllPokemon = fetchAllPokemon;
+  window.requestAllPokemon = requestAllPokemon;
+  window.selectAllPokemon = selectAllPokemon;
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   const root = document.getElementById('root');
   ReactDOM.render(<h1>Pokedex!</h1>, root);
 });
